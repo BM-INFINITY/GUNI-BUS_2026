c717e9964491e53455eef8424d4e92ce3ff4c811
DhruvPatel010-cyber322 <dp0895653@gmail.com>
2026-03-23 21:21:28 +0530

    fix: increase payload limit for profile photo upload

--- server/server.js
+++ server/server.js
@@ -37,8 +37,8 @@ app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 // Global Request Logger
 app.use((req, res, next) => {
